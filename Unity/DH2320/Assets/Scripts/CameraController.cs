ad9f111b759b1f9f856cfe93bb3bd2c28ad338b1
lambertsson <lambert002@gmail.com>
2014-10-21 14:10:05 +0200

    First upload of Unity project

---
+++ Unity/DH2320/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal");
+		float i = Input.GetAxis ("Vertical");
+
+		float x = this.transform.position.x;
+		float y = this.transform.position.y;
+
+		// Minus för att vi flyttar kartan, inte kameran
+		this.transform.position = new Vector2(x-(h*0.2f),y-(i*0.2f));
+
+		if(Input.GetKey("x")){
+			float s = 0.01f;
+
+			x = this.transform.localScale.x;
+			y = this.transform.localScale.y;
+			this.transform.localScale = new Vector2(x + s, y + s);
+			}
+		if(Input.GetKey("z")){
+			float s = 0.01f;
+
+			x = this.transform.localScale.x;
+			y = this.transform.localScale.y;
+			this.transform.localScale = new Vector2(x - s, y - s);
+		}
+	}
+}
---
+++ Unity/DH2320/Assets/Scripts/Tweet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tweet : MonoBehaviour {
+
+	static double MapPixels = 1024;
+	GameObject WorldMap;
+	CoordinateCorverter CC;
+	CoordinateCorverter.CoordinateXY c;
+
+	double Latitude;
+    double Longitude;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void Build(double Latitude, double Longitude){
+		this.Latitude = Latitude;
+		this.Longitude = Longitude;
+		WorldMap = GameObject.FindGameObjectWithTag("Respawn");
+		CC = new CoordinateCorverter();
+		c = CC.Convert (Latitude, Longitude);
+		PinDot(c);
+		this.transform.parent = WorldMap.transform;
+	}
+
+	void PinDot(CoordinateCorverter.CoordinateXY xy){
+		float x = (float)((xy.x - (MapPixels/2))* this.transform.localScale.x * WorldMap.transform.localScale.x);
+		float y = (float)((xy.y - (MapPixels/2))* this.transform.localScale.y * WorldMap.transform.localScale.y);
+		this.transform.position = new Vector2(x,-y);
+	}
+}
