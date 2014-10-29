c9d82483107d9cb627779aea6cf5f37b881d7e29
lambertsson <lambert002@gmail.com>
2014-10-29 12:51:47 +0100

    Here we go

---
+++ Unity/DH2320/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject Dot;
+
+	// Use this for initialization
+	void Start () {
+		GameObject o = (GameObject)Instantiate(Dot);
+		o.GetComponentInChildren<Tweet>().Build(35.21,53.22);
+		o = (GameObject)Instantiate(Dot);
+		o.GetComponentInChildren<Tweet>().Build(21.21,43.22);
+		o = (GameObject)Instantiate(Dot);
+		o.GetComponentInChildren<Tweet>().Build(-31.21,-21.22);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/DH2320/Assets/Scripts/TweetData.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+public class TweetData
+{
+		public string text;
+		public IList<string> hashTags;
+
+		public void fillTweetWithData (JSONObject jsonObject)
+		{
+
+				this.text = jsonObject.GetField ("text").ToString ();
+				Debug.Log (jsonObject.GetField ("entities").type);
+				if (jsonObject.GetField ("entities").type == JSONObject.Type.OBJECT) {
+						Debug.Log ("were inside array");
+						this.hashTags = new List<string> ();
+						JSONObject hashTagList = jsonObject.GetField ("entities").list [0];
+						foreach (JSONObject j in hashTagList.list) {
+								Debug.Log ("J IS " + j.ToString ());
+
+								this.hashTags.Add (j.GetField ("text").ToString ());
+						}
+//
+//			for(int i = 0; i < jsonObject.GetField("entities").list; i++){
+//				string key = (string)obj.keys[i];
+//				JSONObject j = (JSONObject)obj.list[i];
+//				Debug.Log(key);
+//				accessData(j);
+//			}
+				}
+
+
+		}
+	
+		public void printDesc ()
+		{
+				Debug.Log ("tweet is " + this.text);
+				Debug.Log ("hashtag1 is " + this.hashTags [0]);
+
+		}
+}
