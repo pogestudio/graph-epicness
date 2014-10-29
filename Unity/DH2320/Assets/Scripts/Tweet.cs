b06ffaea18f748bd08e7808c8fdc1f1af1887742
lambertsson <lambert002@gmail.com>
2014-10-29 13:31:39 +0100

    Nice

--- Unity/DH2320/Assets/Scripts/Tweet.cs
+++ Unity/DH2320/Assets/Scripts/Tweet.cs
@@ -1,79 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tweet : MonoBehaviour {
+public class Tweet : MonoBehaviour
+{
 
-	static double MapPixels = 1024;
-	GameObject WorldMap;
-	CoordinateCorverter CC;
-	CoordinateCorverter.CoordinateXY c;
+		static double MapPixels = 1024;
+		GameObject WorldMap;
+		CoordinateCorverter CC;
+		CoordinateCorverter.CoordinateXY c;
+		TweetData Data;
 
-	double Latitude;
-    double Longitude;
+		double Latitude;
+		double Longitude;
 
-	// Use this for initialization
-	void Start () {
-	}
-
-	public void Build(double Latitude, double Longitude){
-		this.Latitude = Latitude;
-		this.Longitude = Longitude;
-		WorldMap = GameObject.FindGameObjectWithTag("Respawn");
-		CC = new CoordinateCorverter();
-		c = CC.Convert (Latitude, Longitude);
-		PinDot(c);
-		this.transform.parent = WorldMap.transform;
-	}
-
-	void PinDot(CoordinateCorverter.CoordinateXY xy){
-		float x = (float)((xy.x - (MapPixels/2))* this.transform.localScale.x * WorldMap.transform.localScale.x);
-		float y = (float)((xy.y - (MapPixels/2))* this.transform.localScale.y * WorldMap.transform.localScale.y);
-		this.transform.position = new Vector2(x,-y);
-	}
-
-//    void TryJSON()
-//    {
-//		string JSONString = "{ \"id\" : 524850056202321900, \"text\" : \"Developed countries, please take note! Cuba sending doctors to Liberia to help fight #ebola #EbolaOutbreak thats solidarity. No quarantine!\", \"in_reply_to_status_id\" : \"\", \"in_reply_to_user_id\" : \"\", \"coordinates\" : { \"type\" : \"Point\", \"coordinates\" : [  9.756636,  4.083536 ] }, \"place\" : { \"place_type\" : \"country\", \"full_name\" : \"Cameroon\", \"country\" : \"Cameroon\", \"bounding_box\" : { \"type\" : \"Polygon\", \"coordinates\" : [ 	[ 	[ 	8.4737697, 	1.6525963 ], 	[ 	8.4737697, 	13.0848048 ], 	[ 	16.1912099, 	13.0848048 ], 	[ 	16.1912099, 	1.6525963 ] ] ] } }, \"entities\" : { \"hashtags\" : [ 	{ 	\"text\" : \"ebola\", 	\"indices\" : [ 	85, 	91 ] }, 	{ 	\"text\" : \"EbolaOutbreak\", 	\"indices\" : [ 	92, 	106 ] } ] }, \"favorited\" : false, \"retweeted\" : false, \"lang\" : \"en\", \"timestamp_ms\" : 1413968979304}";
-//		JSONObject j = new JSONObject(JSONString);
-//		print(j.GetField("id"));
-//		print(j.GetField("text"));
-//		print(j.GetField("entities").GetField("hashtags"));
-//		//accessData(j);
-//		//access data (and print it)
-//
-//
-////        TweetData JSONTweet = new TweetData();
-////        JSONTweet = JsonConvert.DeserializeObject<TweetData>(JSONString);
-////        print(JSONTweet.getText());
-//    }
-	void accessData(JSONObject obj){
-		switch(obj.type){
-		case JSONObject.Type.OBJECT:
-			for(int i = 0; i < obj.list.Count; i++){
-				string key = (string)obj.keys[i];
-				JSONObject j = (JSONObject)obj.list[i];
-				Debug.Log(key);
-				accessData(j);
-			}
-			break;
-		case JSONObject.Type.ARRAY:
-			foreach(JSONObject j in obj.list){
-				accessData(j);
-			}
-			break;
-		case JSONObject.Type.STRING:
-			Debug.Log(obj.str);
-			break;
-		case JSONObject.Type.NUMBER:
-			Debug.Log(obj.n);
-			break;
-		case JSONObject.Type.BOOL:
-			Debug.Log(obj.b);
-			break;
-		case JSONObject.Type.NULL:
-			Debug.Log("NULL");
-			break;
-			
+		public void Build (double Latitude, double Longitude)
+		{
+				this.Latitude = Latitude;
+				this.Longitude = Longitude;
+				WorldMap = GameObject.FindGameObjectWithTag ("Respawn");
+				CC = new CoordinateCorverter ();
+				c = CC.Convert (Latitude, Longitude);
+				PinDot (c);
+				this.transform.parent = WorldMap.transform;
 		}
-	}
-	}
+
+		void PinDot (CoordinateCorverter.CoordinateXY xy)
+		{
+				float x = (float)((xy.x - (MapPixels / 2)) * this.transform.localScale.x * WorldMap.transform.localScale.x);
+				float y = (float)((xy.y - (MapPixels / 2)) * this.transform.localScale.y * WorldMap.transform.localScale.y);
+				this.transform.position = new Vector2 (x, -y);
+		}
+}
--- Unity/DH2320/Assets/Scripts/TweetData.cs
+++ Unity/DH2320/Assets/Scripts/TweetData.cs
@@ -7,30 +7,41 @@ using System.Collections.Generic;
 
 public class TweetData
 {
+		public struct CoordinateLL
+		{
+				public double Latitude;
+				public double Longitude;
+		}
+
 		public string text;
+		public CoordinateLL coordinate;
 		public IList<string> hashTags;
+
+
 
 		public void fillTweetWithData (JSONObject jsonObject)
 		{
 
 				this.text = jsonObject.GetField ("text").ToString ();
 				Debug.Log (jsonObject.GetField ("entities").type);
+				if (jsonObject.GetField ("coordinates").type == JSONObject.Type.OBJECT) {
+						//Debug.Log ("jsonobject isS " + jsonObject.GetField ("coordinates").list [1]);
+
+						this.coordinate = new CoordinateLL ();
+						JSONObject list = jsonObject.GetField ("coordinates").list [1];
+						//Debug.Log ("list isss. " + list);
+						this.coordinate.Latitude = Convert.ToDouble (list [0].ToString ());
+						//Debug.Log (list);
+						this.coordinate.Longitude = Convert.ToDouble (list [1].ToString ());
+						//Debug.Log ("coordinates:::: " + this.coordinate.Latitude + "  " + this.coordinate.Longitude);
+				}
 				if (jsonObject.GetField ("entities").type == JSONObject.Type.OBJECT) {
-						Debug.Log ("were inside array");
 						this.hashTags = new List<string> ();
 						JSONObject hashTagList = jsonObject.GetField ("entities").list [0];
 						foreach (JSONObject j in hashTagList.list) {
-								Debug.Log ("J IS " + j.ToString ());
-
+								//Debug.Log ("J IS " + j.ToString ());
 								this.hashTags.Add (j.GetField ("text").ToString ());
 						}
-//
-//			for(int i = 0; i < jsonObject.GetField("entities").list; i++){
-//				string key = (string)obj.keys[i];
-//				JSONObject j = (JSONObject)obj.list[i];
-//				Debug.Log(key);
-//				accessData(j);
-//			}
 				}
 
 
@@ -40,6 +51,7 @@ public class TweetData
 		{
 				Debug.Log ("tweet is " + this.text);
 				Debug.Log ("hashtag1 is " + this.hashTags [0]);
+				Debug.Log ("coordinates are:::: " + this.coordinate.Latitude + "  " + this.coordinate.Longitude);
 
 		}
 }
