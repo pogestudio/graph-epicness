d898cd5ae95443b4a6586137cce9b8aa4d8d6cb4
lambertsson <lambert002@gmail.com>
2014-10-29 11:03:22 +0100

    Unpdate

--- Unity/DH2320/Assets/Scripts/Tweet.cs
+++ Unity/DH2320/Assets/Scripts/Tweet.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts;
+using Newtonsoft.Json;
 
 public class Tweet : MonoBehaviour {
 
@@ -31,4 +33,13 @@ public class Tweet : MonoBehaviour {
 		float y = (float)((xy.y - (MapPixels/2))* this.transform.localScale.y * WorldMap.transform.localScale.y);
 		this.transform.position = new Vector2(x,-y);
 	}
+
+    void TryJSON()
+    {
+        string JSONString = @"{ 'id' : 524850056202321900, 'text' : 'Developed countries, please take note! Cuba sending doctors to Liberia to help fight #ebola #EbolaOutbreak that's solidarity. No quarantine!', 'in_reply_to_status_id' : null, 'in_reply_to_user_id' : null, 'coordinates' : { 'type' : 'Point', 'coordinates' : [  9.756636,  4.083536 ] }, 'place' : { 'place_type' : 'country', 'full_name' : 'Cameroon', 'country' : 'Cameroon', 'bounding_box' : { 'type' : 'Polygon', 'coordinates' : [ 	[ 	[ 	8.4737697, 	1.6525963 ], 	[ 	8.4737697, 	13.0848048 ], 	[ 	16.1912099, 	13.0848048 ], 	[ 	16.1912099, 	1.6525963 ] ] ] } }, 'entities' : { 'hashtags' : [ 	{ 	'text' : 'ebola', 	'indices' : [ 	85, 	91 ] }, 	{ 	'text' : 'EbolaOutbreak', 	'indices' : [ 	92, 	106 ] } ] }, 'favorited' : false, 'retweeted' : false, 'lang' : 'en', 'timestamp_ms' : 1413968979304, 'created_at' : ISODate('2014-10-22T09:09:39.418Z'), '_id' : ObjectId('54477453ea3137aa1a717546') }
+";
+        TweetData JSONTweet = new TweetData();
+        JSONTweet = JsonConvert.DeserializeObject<TweetData>(JSONString);
+        print(JSONTweet.getText());
+    }
 }
---
+++ Unity/DH2320/Assets/Scripts/TweetData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class TweetData
+    {
+        string text;
+        public string getText()
+        {
+            return this.text;
+        }
+    }
+}
